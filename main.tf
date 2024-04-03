4d7530f905741b8b35b5b353dd932cb10e1b8d47
root <root@DESKTOP-DPG6VVM>
2024-04-03 12:51:21 +0530

    ec2_recource

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.43.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_instance" "web" {
+
+    ami = "ami-007020fd9c84e18c7"
+    region = "ap-south-1"
+    instance_type = "t2.micro"
+    count = 1
+
+    tags = {
+        key_name = "kk"
+    }
+}
