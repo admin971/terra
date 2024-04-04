9acd0a67f1e552504c5db77b3375122885729818
karthick <mailkarthi90@gmail.com>
2024-04-04 20:23:27 +0530

    ec2codeupdate

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.43.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_instance" "web" {
+
+    ami = "ami-007020fd9c84e18c7"
+    region = "ap-south-1"
+    instance_type = "t2.micro"
+    count = 1
+
+    resource "aws_key_pair" "aws_key" {
+  key_name   = "kk"
+  public_key = "aprilpemkey"
+}
+
+    tags = {
+        key_name = "kk"
+    }
+}
